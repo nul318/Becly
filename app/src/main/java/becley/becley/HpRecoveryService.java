4e13d4742fa7317c31848d3c69d45b5fb51b70c6
DongHoon <moderate41@gmail.com>
2016-12-17 04:29:26 +0900

    50%

---
+++ app/src/main/java/becley/becley/HpRecoveryService.java
@@ -0,0 +1,140 @@
+package becley.becley;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+
+/**
+ * Created by DongHoon on 2016-12-16.
+ */
+
+public class HpRecoveryService extends Service {
+
+    LocalBroadcastManager aa = LocalBroadcastManager.getInstance(this);
+    Runnable hpRecovery ;
+    Thread hpRecoveryThread ;
+    public static final int BASIC_ATTACK = 10;
+    public static final int STRIKE_ATTACK = 30;
+    public static int hp=100;
+    public static boolean isStop = true;
+    public static int thread_val = 0;
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // TODO Auto-generated method stub
+
+        sendMessage();
+
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("basic attack");
+        intentFilter.addAction("strike sttack");
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                enemy_attack, intentFilter);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(
+                enemy_attack);
+    }
+
+    // Send an Intent with an action named "custom-event-name". The Intent
+    // sent should
+    // be received by the ReceiverActivity.
+    private void sendMessage() {
+        hpRecovery = new Runnable() {
+            @Override
+            public void run() {
+                while(isStop) {
+                    if(hp<100) {
+                        Intent hp_gen_intent = new Intent("custom-event-name");
+                        // You can also include some extra data.
+                        hp_gen_intent.putExtra("message", hp++);
+                        aa.sendBroadcast(hp_gen_intent);
+                    }
+                    else
+                    {
+                        hp= 100;
+                        Intent hp_gen_intent = new Intent("custom-event-name");
+                        // You can also include some extra data.
+                        hp_gen_intent.putExtra("message", hp);
+                        aa.sendBroadcast(hp_gen_intent);
+                    }
+                    thread_val++;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+
+
+
+        if(thread_val==0) {
+            hpRecoveryThread = new Thread(hpRecovery);
+            if (Thread.State.NEW == hpRecoveryThread.getState()) {
+                hpRecoveryThread.start();
+            }
+        }
+
+    }
+
+
+    private BroadcastReceiver enemy_attack = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+            // Get extra data included in the Intent
+            if(intent.getAction().equals("basic attack")==true)
+            {
+
+                hp = hp-BASIC_ATTACK;
+                if(hp<0)
+                {
+                    hp=0;
+                }
+                Intent hp_sub_intent = new Intent("custom-event-name");
+                // You can also include some extra data.
+                hp_sub_intent.putExtra("message", hp);
+                aa.sendBroadcast(hp_sub_intent);
+
+            }
+            else if(intent.getAction().equals("strike sttack")==true)
+            {
+                hp = hp-STRIKE_ATTACK;
+                if(hp<0)
+                {
+                    hp=0;
+                }
+                Intent hp_sub_intent = new Intent("custom-event-name");
+                // You can also include some extra data.
+                hp_sub_intent.putExtra("message", hp);
+                aa.sendBroadcast(hp_sub_intent);
+
+            }
+        }
+    };
+}
--- app/src/main/java/becley/becley/MainActivity.java
+++ app/src/main/java/becley/becley/MainActivity.java
@@ -2,21 +2,50 @@ package becley.becley;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView User_HP;
+    LocalBroadcastManager aa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User_HP = (TextView) findViewById(R.id.User_HP);
+        aa = LocalBroadcastManager.getInstance(MainActivity.this);
+
         if(!isServiceRunningCheck()) {
             Intent intent = new Intent("becley.becley.shakeservice");
-//            intent.setPackage("becley.becley");
+            intent.setPackage("becley.becley");
             startService(intent);
         }
+        Button buttonStartService = (Button)findViewById(R.id.btnPlay);
+        buttonStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //Register MessageService in Manifest to work
+                Intent hp_gen_intent = new Intent("strike sttack");
+                // You can also include some extra data.
+                hp_gen_intent.setAction("strike sttack");
+                aa.sendBroadcast(hp_gen_intent);
+            }
+        });
+        HpRecoveryService.isStop = true;
+        startService(new Intent(MainActivity.this, HpRecoveryService.class));
     }
 
     public boolean isServiceRunningCheck() {
@@ -29,4 +58,40 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    @Override
+    protected void onPause() {
+        // Unregister since the activity is paused.
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(
+                mMessageReceiver);
+        super.onPause();
+    }
+    protected  void onDestroy(){
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        // Register to receive messages.
+        // We are registering an observer (mMessageReceiver) to receive Intents
+        // with actions named "custom-event-name".
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                mMessageReceiver, new IntentFilter("custom-event-name"));
+        super.onResume();
+    }
+
+    // Our handler for received Intents. This will be called whenever an Intent
+    // with an action named "custom-event-name" is broadcasted.
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+            // Get extra data included in the Intent
+            int message = intent.getIntExtra("message",100);
+            User_HP.setText(""+message);
+        }
+    };
+
 }
+
+
